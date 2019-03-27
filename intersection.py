bf6ae376d15a5ff6a1830742ef77f12cde0426c9
maxman365 <46762721+maxman365@users.noreply.github.com>
2019-03-27 18:39:50 +0100

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,52 @@
+from kit_calcul_vecteur import calcule_normales_triangle
+from kit_calcul_vecteur import calcule_normale
+from kit_calcul_vecteur import calcule_vecteurs_triangle
+from kit_calcul_vecteur import calcule_vecteur
+from kit_calcul_vecteur import calcule_ensemble_des_cotes_des_triangles
+from kit_calcul_vecteur import calcule_produit_scalaire
+
+
+def verifie_itersection(vecteur_segment_centre, placement_personnage, vecteur_directeur_segment, point_segment):
+    """calcule les T pour que les segment s'intersectent, retourne True si les segments ne se coupent pas"""
+    ray_x = vecteur_segment_centre[0]
+    ray_y = vecteur_segment_centre[1]
+    segm_x = vecteur_directeur_segment[0]
+    segm_y = vecteur_directeur_segment[1]
+    scalaire_carré = (calcule_produit_scalaire(vecteur_segment_centre, vecteur_directeur_segment)) ** 2
+    norme_vecteur_segment_centre = ray_x ** 2 + ray_y ** 2
+    norme_vecteur_directeur_segment = segm_x ** 2 + segm_y ** 2
+    if scalaire_carré == norme_vecteur_directeur_segment * norme_vecteur_segment_centre:
+        return True
+    else:
+        numéra = ray_x * (point_segment[1] - placement_personnage[1]) - ray_y * (
+                    point_segment[0] - placement_personnage[0])
+        dénomin = ray_y * segm_x - segm_y * ray_x
+        T2 = numéra / dénomin
+
+        if ray_x != 0 and ray_y == 0 or ray_x != 0 and ray_y != 0:
+            T1 = (segm_x * T2 + point_segment[0] - placement_personnage[0]) / ray_x
+
+        if ray_x == 0 and ray_y != 0:
+            T1 = (segm_y * T2 - placement_personnage[1] + point_segment[1])
+
+        if ray_x == 0 and ray_y == 0:
+            return True
+
+        if T1 >= 0 and 0 < T2 < 1:
+            return False
+
+        return True
+
+
+def visible(dictionnaire_de_vecteur, emplacement_personnage, point_a_verifier):
+    # il faut appliquer les fonction calcule_ensemble_des_côtés_des_triangles d'abord pour avoir le dictionnaire
+    vecteur_segment_centre = calcule_vecteur(emplacement_personnage, point_a_verifier)
+    for i in dictionnaire_de_vecteur:
+        print(verifie_itersection(vecteur_segment_centre, emplacement_personnage, dictionnaire_de_vecteur[i], i))
+        if not(verifie_itersection(vecteur_segment_centre, emplacement_personnage, dictionnaire_de_vecteur[i], i)):
+            return False
+    return True
+
+
+
+
---
+++ kit_calcul_vecteur.py
@@ -0,0 +1,48 @@
+
+def calcule_vecteur(point1, point2):
+    """prend deux listes  avec les coordonnées des points et retourne les coordonnées du vecteur"""
+    x_vecteur = point2[0] - point1[0]
+    y_vecteur = point2[1] - point1[1]
+    return [x_vecteur, y_vecteur]
+
+
+def calcule_produit_scalaire(vecteur1, vecteur2):
+    return vecteur1[0] * vecteur2[0] + vecteur1[1] * vecteur2[1]
+
+
+def calcule_vecteurs_triangle(triangle):
+    d = dict()
+    vecteur1 = calcule_vecteur(triangle[0], triangle[1])
+    d[triangle[0]] = vecteur1
+    vecteur2 = calcule_vecteur(triangle[1], triangle[2])
+    d[triangle[1]] = vecteur2
+    vecteur3 = calcule_vecteur(triangle[2], triangle[0])
+    d[triangle[2]] = vecteur3
+    return d
+
+
+def calcule_ensemble_des_cotes_des_triangles(liste_de_triangle):
+    """calcule les vecteurs de côtés de tous les triangles d'une liste et retourne un dico avec unpoint comme clef et le vecteur comme valeur"""
+    dictionnaire_des_vecteurs = dict()
+    for i in liste_de_triangle:
+        dictionnaire_des_vecteurs_intermediaire = calcule_vecteurs_triangle(i)
+        dictionnaire_des_vecteurs.update(dictionnaire_des_vecteurs_intermediaire)
+    return dictionnaire_des_vecteurs
+
+
+# fonctions inutiles
+
+
+def calcule_normale(liste):  # pour un vecteur orienté vers le haut la normale est orientée vers la gauche
+    """prend une  liste des coordonnées d'un vecteur et retourne sa normale"""
+    x_normale = -liste[1]
+    y_normale = liste[0]
+    return [x_normale, y_normale]
+
+
+def calcule_normales_triangle(liste_de_liste):  # les triangles doivent être rentrés dans le sens horaire pour avoir les normales à l'exterieur
+    """prend un triangle et retourne les coordonnées de ses normales dans l'ordre des points"""
+    normale_1 = calcule_normale(calcule_vecteur(liste_de_liste[0], liste_de_liste[1]))
+    normale_2 = calcule_normale(calcule_vecteur(liste_de_liste[1], liste_de_liste[2]))
+    normale_3 = calcule_normale(calcule_vecteur(liste_de_liste[2], liste_de_liste[0]))
+    return [normale_1, normale_2, normale_3]
